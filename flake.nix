47c9ccb21646f2a704d2d92a55d63ffe974c7464
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-06-20 12:16:46 +0200

    chore(dev): update flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "Cosmwasm VM";
   inputs = {
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/60ddbcfc9e5f02f97564fa01a5646b62d82e0756";
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/60ddbcfc9e5f02f97564fa01a5646b62d82e0756";
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,35 +12,28 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, fenix }:
+  outputs = { self, nixpkgs, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ fenix.overlay ];
-            config.allowUnfree = true;
-          };
-          pkgs-unstable = import nixpkgs-unstable {
-            inherit system;
-            overlays = [ fenix.overlay ];
-            config.allowUnfree = true;
-          };
-      in
-        rec {
-          devShell = pkgs.mkShell {
-            buildInputs = [
-              pkgs.pkg-config
-              pkgs.openssl
-              pkgs.stdenv.cc.cc
-              (pkgs-unstable.fenix.latest.withComponents [
-                "cargo"
-                "clippy"
-                "rust-src"
-                "rustc"
-                "rustfmt"
-              ])
-            ];
-            RUST_LOG="debug";
-          };
-        }
-    );
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlay ];
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.pkg-config
+            pkgs.openssl
+            pkgs.stdenv.cc.cc
+            (pkgs.fenix.latest.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+          ];
+          RUST_LOG = "debug";
+        };
+      });
 }
