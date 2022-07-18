67efa8333a3ec4e9b9a5cc29b77dba11ecce0d33
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-07-18 14:06:21 +0200

    feat(ci): simple nix flake to test/build the project

--- flake.nix
+++ flake.nix
@@ -1,39 +1,48 @@
 {
   description = "Cosmwasm VM";
   inputs = {
-    nixpkgs.url =
-      "github:NixOS/nixpkgs/60ddbcfc9e5f02f97564fa01a5646b62d82e0756";
+    nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    fenix = {
-      url = "github:nix-community/fenix";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, flake-utils, fenix }:
+  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ fenix.overlay ];
+          overlays = [ rust-overlay.overlays.default ];
         };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.pkg-config
-            pkgs.openssl
-            pkgs.stdenv.cc.cc
-            (pkgs.fenix.latest.withComponents [
-              "cargo"
-              "clippy"
-              "rust-src"
-              "rustc"
-              "rustfmt"
-            ])
-          ];
-          RUST_LOG = "debug";
-        };
+      in with pkgs;
+      let
+        # Nightly rust used for wasm runtime compilation
+        rust-nightly = rust-bin.nightly.latest.default;
+
+        # Crane lib instantiated with current nixpkgs
+        crane-lib = crane.mkLib pkgs;
+
+        # Crane pinned to nightly Rust
+        crane-nightly = crane-lib.overrideToolchain rust-nightly;
+      in rec {
+        packages.cosmwasm-vm = crane-nightly.buildPackage ({
+          src = lib.cleanSourceWith {
+            filter = lib.cleanSourceFilter;
+            src = lib.cleanSourceWith {
+              filter = nix-gitignore.gitignoreFilterPure (name: type: true)
+                [ ./.gitignore ] ./.;
+              src = ./.;
+            };
+          };
+        });
+        packages.default = packages.cosmwasm-vm;
       });
 }
