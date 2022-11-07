317737441c9c47776a7e08de22b1d92d7399f7b3
aeryz <abdullaheryz@protonmail.com>
2022-11-07 13:25:33 +0100

    feat(code_gen): add extra functions and custom instructions
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- vm-wasmi/src/code_gen.rs
+++ vm-wasmi/src/code_gen.rs
@@ -1,3 +1,4 @@
+use alloc::string::String;
 use alloc::{vec, vec::Vec};
 use cosmwasm_minimal_std::{ContractResult, Empty, Response};
 use wasm_instrument::parity_wasm::{
@@ -12,6 +13,7 @@ pub struct ModuleDefinition {
     execute_call: ExecuteCall,
     migrate_call: MigrateCall,
     query_call: QueryCall,
+    additional_functions: Vec<Function>,
     additional_binary_size: usize,
 }
 
@@ -27,14 +29,94 @@ pub enum Error {
 }
 
 impl ModuleDefinition {
-    pub fn new(additional_binary_size: usize) -> Result<Self, Error> {
+    pub fn new(
+        additional_functions: Vec<Function>,
+        additional_binary_size: usize,
+    ) -> Result<Self, Error> {
         Ok(Self {
             instantiate_call: InstantiateCall::new().map_err(|_| Error::Internal)?,
             execute_call: ExecuteCall::new().map_err(|_| Error::Internal)?,
             migrate_call: MigrateCall::new().map_err(|_| Error::Internal)?,
             query_call: QueryCall::new().map_err(|_| Error::Internal)?,
+            additional_functions,
             additional_binary_size,
         })
+    }
+
+    pub fn with_instructions<F: Into<String>>(
+        fn_name: F,
+        mut instructions: Vec<Instruction>,
+        additional_binary_size: usize,
+    ) -> Result<Self, Error> {
+        instructions.push(Instruction::End);
+        Self::new(
+            vec![Function {
+                name: fn_name.into(),
+                params: Vec::new(),
+                result: None,
+                definition: FuncBody::new(Vec::new(), Instructions::new(instructions)),
+            }],
+            additional_binary_size,
+        )
+    }
+}
+
+#[derive(Debug)]
+pub struct Function {
+    pub name: String,
+    pub params: Vec<ValueType>,
+    pub result: Option<ValueType>,
+    pub definition: FuncBody,
+}
+
+impl Function {
+    pub fn instructions(&self) -> &Instructions {
+        self.definition.code()
+    }
+}
+
+pub struct FunctionBuilder(Function);
+
+impl FunctionBuilder {
+    pub fn new<S: Into<String>>(name: S) -> Self {
+        Self(Function {
+            name: name.into(),
+            params: Vec::new(),
+            result: None,
+            definition: FuncBody::empty(),
+        })
+    }
+
+    pub fn param(mut self, param: ValueType) -> Self {
+        self.0.params.push(param);
+        self
+    }
+
+    pub fn result(mut self, result: ValueType) -> Self {
+        self.0.result = Some(result);
+        self
+    }
+
+    pub fn definition(mut self, func_body: FuncBody) -> Self {
+        self.0.definition = func_body;
+        self
+    }
+
+    pub fn build(self) -> Function {
+        self.0
+    }
+
+    pub fn local(mut self, count: u32, value_type: ValueType) -> Self {
+        self.0
+            .definition
+            .locals_mut()
+            .push(Local::new(count, value_type));
+        self
+    }
+
+    pub fn instructions(mut self, instructions: Vec<Instruction>) -> Self {
+        *self.0.definition.code_mut() = Instructions::new(instructions);
+        self
     }
 }
 
@@ -179,17 +261,10 @@ impl From<ModuleDefinition> for WasmModule {
             .memory(0)
             .build();
 
-        struct Function {
-            name: &'static str,
-            params: Vec<ValueType>,
-            result: Option<ValueType>,
-            definition: FuncBody,
-        }
-
-        let function_definitions = vec![
+        let mut function_definitions = vec![
             // fn allocate(size: usize) -> u32;
             Function {
-                name: "allocate",
+                name: "allocate".into(),
                 params: vec![ValueType::I32],
                 result: Some(ValueType::I32),
                 definition: FuncBody::new(
@@ -233,42 +308,42 @@ impl From<ModuleDefinition> for WasmModule {
             },
             // fn instantiate(env_ptr: u32, info_ptr: u32, msg_ptr: u32) -> u32;
             Function {
-                name: "instantiate",
+                name: "instantiate".into(),
                 params: vec![ValueType::I32, ValueType::I32, ValueType::I32],
                 result: Some(ValueType::I32),
                 definition: def.instantiate_call.0,
             },
             // fn execute(env_ptr: u32, info_ptr: u32, msg_ptr: u32) -> u32;
             Function {
-                name: "execute",
+                name: "execute".into(),
                 params: vec![ValueType::I32, ValueType::I32, ValueType::I32],
                 result: Some(ValueType::I32),
                 definition: def.execute_call.0,
             },
             // fn migrate(env_ptr: u32, msg_ptr: u32) -> u32;
             Function {
-                name: "migrate",
+                name: "migrate".into(),
                 params: vec![ValueType::I32, ValueType::I32],
                 result: Some(ValueType::I32),
                 definition: def.migrate_call.0,
             },
             // fn deallocate(pointer: u32);
             Function {
-                name: "deallocate",
+                name: "deallocate".into(),
                 params: vec![ValueType::I32],
                 result: None,
                 definition: FuncBody::new(Vec::new(), Instructions::empty()),
             },
             // fn query(env_ptr: u32, msg_ptr: u32) -> u32;
             Function {
-                name: "query",
+                name: "query".into(),
                 params: vec![ValueType::I32, ValueType::I32],
                 result: Some(ValueType::I32),
                 definition: def.query_call.0,
             },
             // dummy function to increase the binary size
             Function {
-                name: "dummy_fn",
+                name: "dummy_fn".into(),
                 params: vec![],
                 result: None,
                 definition: FuncBody::new(
@@ -282,12 +357,16 @@ impl From<ModuleDefinition> for WasmModule {
             },
             // fn interface_version_8() -> ();
             Function {
-                name: "interface_version_8",
+                name: "interface_version_8".into(),
                 params: vec![],
                 result: None,
                 definition: FuncBody::new(Vec::new(), Instructions::empty()),
             },
         ];
+
+        for function in def.additional_functions {
+            function_definitions.push(function);
+        }
 
         for (i, func) in function_definitions.into_iter().enumerate() {
             let mut signature_builder = contract.function().signature();
@@ -302,7 +381,7 @@ impl From<ModuleDefinition> for WasmModule {
                 .with_body(func.definition)
                 .build()
                 .export()
-                .field(func.name)
+                .field(func.name.as_str())
                 .internal()
                 .func(i as u32)
                 .build();
